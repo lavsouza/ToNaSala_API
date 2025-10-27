18b64fb3b0ff8a96592dfbd81accdb06468dc08d
Lucas André <lavs1@discente.ifpe.edu.br>
2025-10-27 15:33:16 -0300

    First commit

---
+++ src/main/java/com/tonasala/tonasala_api/ToNaSalaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tonasala.tonasala_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToNaSalaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToNaSalaApiApplication.class, args);
+    }
+
+}
