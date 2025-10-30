74367307e9faac65c092285e23869f61d6fedff6
Tássio Tristão <tassio.tristao05@gmail.com>
2025-10-30 15:28:31 -0300

    correção das importacoes

--- src/main/java/com/tonasala/tonasala_api/repository/GuardianRepository.java
+++ src/main/java/com/tonasala/tonasala_api/repository/GuardianRepository.java
@@ -1,9 +1,11 @@
 package com.tonasala.tonasala_api.repository;
 
-import com.tonasala.tonasala_api.entity.Institution;
+import com.tonasala.tonasala_api.entity.Guardian;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface GuardianRepository extends JpaRepository<Institution, Long> {
+import java.util.Optional;
+
+public interface GuardianRepository extends JpaRepository<Guardian, Long> {
+    boolean existsByEmail(String email);
+    Optional<Guardian> findByEmail(String email);
 }
