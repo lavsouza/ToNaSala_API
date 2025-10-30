cb7d364334de8277d69dbc8187dc1151fe06a767
Tássio Tristão <tassio.tristao05@gmail.com>
2025-10-30 14:17:42 -0300

    adicionado os metodos findByEmail e existsByEmail

--- src/main/java/com/tonasala/tonasala_api/repository/InstitutionRepository.java
+++ src/main/java/com/tonasala/tonasala_api/repository/InstitutionRepository.java
@@ -4,6 +4,10 @@ import com.tonasala.tonasala_api.entity.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InstitutionRepository extends JpaRepository<Institution, Long>{
+    boolean existsByEmail(String email);
+    Optional<Institution> findByEmail(String email);
 }
