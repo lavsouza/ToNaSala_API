e863bb7ca1b7b9d57341121268d2878ed1f893c4
Tássio Tristão <tassio.tristao05@gmail.com>
2025-10-30 14:06:36 -0300

    criacao de dtos da instituicao

--- src/main/java/com/tonasala/tonasala_api/dto/institution/InstitutionUpdateDTO.java
+++ src/main/java/com/tonasala/tonasala_api/dto/institution/InstitutionUpdateDTO.java
@@ -1,24 +1,19 @@
 package com.tonasala.tonasala_api.dto.institution;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public record InstitutionCreate(
-        @NotBlank(message = "O nome é obrigatório.")
+public record InstitutionUpdateDTO(
         @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
         String name,
 
-        @NotBlank(message = "O CNPJ é obrigatório.")
         @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter exatamente 14 dígitos numéricos.")
         String cnpj,
 
-        @NotBlank(message = "O e-mail é obrigatório.")
         @Email(message = "Formato de e-mail inválido.")
         String email,
 
-        @NotBlank(message = "A senha é obrigatória.")
         @Size(min = 6, max = 60, message = "A senha deve ter entre 6 e 60 caracteres.")
         String password
 ) {}
