6dc1df02b45811622c1364b0fe7eb3b19494dabf
Tássio Tristão <tassio.tristao05@gmail.com>
2025-10-30 16:19:32 -0300

    adicionado novas funcoes para buscar email

--- src/main/java/com/tonasala/tonasala_api/repository/ProfessorRepository.java
+++ src/main/java/com/tonasala/tonasala_api/repository/ProfessorRepository.java
@@ -4,6 +4,10 @@ import com.tonasala.tonasala_api.entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    boolean existsByEmail(String email);
+    Optional<Professor> findByEmail(String email);
 }
