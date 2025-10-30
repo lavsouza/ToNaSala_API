a90636956d0edc42f358b2f8526d24bf4526075e
Tássio Tristão <tassio.tristao05@gmail.com>
2025-10-30 15:50:24 -0300

    adicionado metodos para validacao do email

--- src/main/java/com/tonasala/tonasala_api/repository/StudentRepository.java
+++ src/main/java/com/tonasala/tonasala_api/repository/StudentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long>{
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
